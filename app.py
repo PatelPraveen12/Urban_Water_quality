4e6167d213b80f6feb5dbf7b1855bdd32ae0172b
PatelPraveen12 <patel567praveen@gmail.com>
2021-09-13 17:04:39 +0530

    updated code

--- app.py
+++ app.py
@@ -15,29 +15,30 @@ def home():
 def predict():
 
     if request.method == "POST":
-            year = request.form["year"]
-    do = request.form["do"]
-    ph = request.form["ph"]
-    nco = request.form["nco"]
-    bod = request.form["bod"]
-    na = request.form["na"]
-    tc = request.form["tc"]
-    total = [[int(year),float(do),float(ph),float(nco),float(bod),float(na),float(tc)]]
-    y_pred = model.predict(total)
-    print(y_pred)
-    y_pred =y_pred[[0]]
-    if(y_pred >= 95 and y_pred <= 100) :
-        return render_template("web.html",showcase = 'Excellent,The predicted value is '+ str(y_pred))
-    elif(y_pred >= 89 and y_pred <= 94) :
-        return render_template("web.html",showcase = 'Very good,The predicted value is '+str(y_pred))
-    elif(y_pred >= 80 and y_pred <= 88) :
-        return render_template("web.html",showcase = 'Good,The predicted value is'+str(y_pred))
-    elif(y_pred >= 65 and y_pred <= 79) :
-        return render_template("web.html",showcase = 'Fair,The predicted value is '+str(y_pred))
-    elif(y_pred >= 45 and y_pred <= 64) :
-        return render_template("web.html",showcase = 'Marginal,The predicted value is '+str(y_pred))
-    else :
-        return render_template("web.html",showcase = 'Poor,The predicted value is '+str(y_pred))
+    
+        year = request.form["year"]
+        do = request.form["do"]
+        ph = request.form["ph"]
+        nco = request.form["nco"]
+        bod = request.form["bod"]
+        na = request.form["na"]
+        tc = request.form["tc"]
+        total = [[int(year),float(do),float(ph),float(nco),float(bod),float(na),float(tc)]]
+        y_pred = model.predict(total)
+        print(y_pred)
+        y_pred =y_pred[[0]]
+        if(y_pred >= 95 and y_pred <= 100) :
+            return render_template("web.html",showcase = 'Excellent,The predicted value is '+ str(y_pred))
+        elif(y_pred >= 89 and y_pred <= 94) :
+            return render_template("web.html",showcase = 'Very good,The predicted value is '+str(y_pred))
+        elif(y_pred >= 80 and y_pred <= 88) :
+            return render_template("web.html",showcase = 'Good,The predicted value is'+str(y_pred))
+        elif(y_pred >= 65 and y_pred <= 79) :
+            return render_template("web.html",showcase = 'Fair,The predicted value is '+str(y_pred))
+        elif(y_pred >= 45 and y_pred <= 64) :
+            return render_template("web.html",showcase = 'Marginal,The predicted value is '+str(y_pred))
+        else :
+            return render_template("web.html",showcase = 'Poor,The predicted value is '+str(y_pred))
 
 
     
